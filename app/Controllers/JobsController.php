b49700a30ef74ce271c9e79e44d9dfccf41afb95
Jasan Hernandez <jasan_azael@hotmail.es>
2018-10-08 18:38:23 -0500

    Usando respect/validation para validar que los campos no sean vacíos

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -1,33 +1,45 @@
 <?php
 namespace App\Controllers;
 use App\Models\{Job, Project};
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController {
   public function getAddJobAction($request) {
+    $responseMessage = null;
     # Si $post esta vacio No guardamos nada, y si no esta vacío entonces lo guardamos.
     // Si Request->getMethod == POST entonces sigue adelante.
     if($request->getMethod() == 'POST') {
       $postData = $request->getParsedBody();
-      if($postData['title'] == '' && $postData['description'] == '') {
+      $jobValidator = v::key('title', v::stringType()->notEmpty())
+                  ->key('description', v::stringType()->notEmpty());
+        try {
+          $jobValidator->assert($postData);
+          if($postData['title'] == '' && $postData['description'] == '') {
 
-      } else {
-        $job = new Job();
-        $job->title = $postData['title'];
-        $job->description = $postData['description'];
-        $job->save();
-      }
-      if($postData['titleProyect'] == '' && $postData['descriptionProyect'] == '') {
+          } else {
+            $job = new Job();
+            $job->title = $postData['title'];
+            $job->description = $postData['description'];
+            $job->save();
+          }
+          if($postData['titleProyect'] == '' && $postData['descriptionProyect'] == '') {
 
-      } else {
-        $project = new Project();
-        $project->title = $postData['titleProyect'];
-        $project->description = $postData['descriptionProyect'];
-        $project->visible = true;
-        $project->save();
-      }
+          } else {
+            $project = new Project();
+            $project->title = $postData['titleProyect'];
+            $project->description = $postData['descriptionProyect'];
+            $project->visible = true;
+            $project->save();
+          }
+          $responseMessage = 'SAVE!!';
+        } catch(\Exception $e) {
+          $responseMessage = $e->getMessage();
+        }
     }
 
-    return $this->renderHTML('addJob.twig');
+    return $this->renderHTML('addJob.twig', [
+      'responseMessage' => $responseMessage,
+    ]);
 
   }
 }
